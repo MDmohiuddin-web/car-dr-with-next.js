f5f31bc09d792ba7873235162bfd3922397484e7
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-10-29 21:12:35 +0600

    add some

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import React from "react";
+import info from "../../../public/assets/Group.svg";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div className="my-10  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+      <Image src={info} alt="" className="mt-5 md:mt-16 " />
+    </div>
+  );
 };
 
 export default page;
