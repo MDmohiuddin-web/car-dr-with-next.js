e65a641251eb7d6aa0f5c4329ac6fbe1e9a90e0a
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-10-31 22:50:18 +0600

    add some

--- src/app/component/Review.jsx
+++ src/app/component/Review.jsx
@@ -5,63 +5,45 @@ import img2 from "../../../public/assets/review2.png";
 
 const Review = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full my-5">
       <div id="review1" className="carousel-item relative w-full">
-        <div className="flex gap-5 my-5 justify-center items-center w-full">
-          <Image src={img1} alt=""></Image>
-          <Image src={img2} alt=""></Image>
+        <div className="flex gap-5 flex-wrap my-5 justify-center items-center w-full">
+          <Image src={img1} alt="Review 1" />
+          <Image src={img2} alt="Review 2" />
         </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a
-            href="#review3"
-            className="btn btn-circle bg-[#616c7d] border-0 text-white"
-          >
+          <a href="#review3" className="btn btn-circle bg-[#616c7d] border-0 text-white">
             ❮
           </a>
-          <a
-            href="#review2"
-            className="btn btn-circle bg-red-500 border-0  text-white "
-          >
+          <a href="#review2" className="btn btn-circle bg-red-500 border-0 text-white">
             ❯
           </a>
         </div>
       </div>
       <div id="review2" className="carousel-item relative w-full">
-        <div className="flex gap-5 my-5 justify-center items-center w-full">
-          <Image src={img2} alt=""></Image>
-          <Image src={img1} alt=""></Image>
+        <div className="flex gap-5 flex-wrap my-5 justify-center items-center w-full">
+          <Image src={img2} alt="Review 2" />
+          <Image src={img1} alt="Review 1" />
         </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a
-            href="#review1"
-            className="btn btn-circle bg-[#616c7d] border-0 text-white"
-          >
+          <a href="#review1" className="btn btn-circle bg-[#616c7d] border-0 text-white">
             ❮
           </a>
-          <a
-            href="#review3"
-            className="btn btn-circle bg-red-500 border-0  text-white "
-          >
+          <a href="#review3" className="btn btn-circle bg-red-500 border-0 text-white">
             ❯
           </a>
         </div>
       </div>
       <div id="review3" className="carousel-item relative w-full">
-        <div className="flex gap-5 my-5 justify-center items-center w-full">
-          <Image src={img2} alt=""></Image>
-          <Image src={img1} alt=""></Image>
+        <div className="flex gap-5 flex-wrap my-5 justify-center items-center w-full">
+          <Image src={img2} alt="Review 2" />
+          <Image src={img1} alt="Review 1" />
         </div>
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a
-            href="#review2"
-            className="btn btn-circle bg-[#616c7d] border-0 text-white"
-          >
+          <a href="#review2" className="btn btn-circle bg-[#616c7d] border-0 text-white">
             ❮
           </a>
-          <a
-            href="#review2"
-            className="btn btn-circle bg-red-500 border-0  text-white "
-          >
+          <a href="#review1" className="btn btn-circle bg-red-500 border-0 text-white">
             ❯
           </a>
         </div>
