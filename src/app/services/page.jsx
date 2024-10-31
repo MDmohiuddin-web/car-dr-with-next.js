5f3444d56f831a83d5defd43b1b47c3bf87213b2
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-10-31 21:48:20 +0600

    contact page set

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -7,7 +7,7 @@ const Service = () => {
 
 
   return (
-    <div className="my-10 flex flex-wrap gap-5 justify-center w-4/5 mx-auto">
+    <div className="my-10 flex flex-wrap gap-5 justify-center w-4/5 mx-auto text-black">
       {/* <h4>Total Services: {services.length}</h4> */} 
       {Services.map((service) => (
         <ServiceCard key={service._id} service={service}  /> 
